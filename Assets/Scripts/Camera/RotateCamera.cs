9903f78b55bd982dd3d359e59443c10223d4e8a0
Joshua <joshuawiranta@hotmail.com>
2021-10-14 12:50:13 +0200

    Cam Rotate

--- Assets/Scripts/Camera/RotateCamera.cs
+++ Assets/Scripts/Camera/RotateCamera.cs
@@ -7,17 +7,36 @@ public class RotateCamera : MonoBehaviour
     [SerializeField]private float mDelta = 10;
     [SerializeField]private float mSpeedRight = 3.0f;
     [SerializeField] private float mSpeedLeft = 3.0f;
+    [SerializeField] private bool lockLeft = default;
+    [SerializeField] private bool lockRight = default;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.mousePosition.x >= Screen.width - mDelta)
+        if(transform.rotation.y >= 0.065f)
         {
-            transform.position += Vector3.right * Time.deltaTime * mSpeedRight;
+            lockRight = true;
+		}    
+        else
+        {
+            lockRight = false;
+		}
+        if( transform.rotation.y <= -0.20f )
+        {
+            lockLeft = true;
         }
-        if(Input.mousePosition.x <= 0f + mDelta)
+        else
         {
-            transform.position += Vector3.left * Time.deltaTime * mSpeedLeft;
+            lockLeft = false;
+        }
+
+        if (Input.mousePosition.x >= Screen.width - mDelta && !lockRight)
+        {
+            transform.Rotate( 0f, mSpeedRight * Time.deltaTime, 0f );
+        }
+        if(Input.mousePosition.x <= 0f + mDelta && !lockLeft)
+        {
+            transform.Rotate(0f, mSpeedLeft * Time.deltaTime, 0f);
         }
     }
 }
